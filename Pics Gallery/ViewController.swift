0f072ace6cfefc7fc162a6e18cb79f997176a60f
Leonid Kuznetcov <leonidkuz18@gmail.com>
2020-10-21 19:00:15 +0300

    Initial Commit

---
+++ Pics Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pics Gallery
+//
+//  Created by Леонид Кузнецов on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pics GalleryTests/Pics_GalleryTests.swift
@@ -0,0 +1,33 @@
+//
+//  Pics_GalleryTests.swift
+//  Pics GalleryTests
+//
+//  Created by Леонид Кузнецов on 21.10.2020.
+//
+
+import XCTest
+@testable import Pics_Gallery
+
+class Pics_GalleryTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
